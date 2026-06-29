655e06fa908c11d8347f77b8340de1137b0022e9
Tejas-Triv <tejastrivedi2007@gmail.com>
2026-06-30 00:20:28 +0530

    Add files via upload

---
+++ Compound Interest Analyzer.py
@@ -0,0 +1,11 @@
+print('Compound Interest Analyser')
+principal=float(input('enter principal amount'))
+r=float(input('enter rate of interest'))
+t=int(input('enter time in years'))
+newprincipal=principal
+interest=0
+for i in range(0,t):
+    newprincipal+=(newprincipal*r)/100
+    interest=(newprincipal*r)/100
+    print('Year-',i+1,'New Principal=',round(newprincipal),'interest acquired this year=',round(interest),'total gains=',round(newprincipal-principal),sep='|')
+print('Initial Principal was:',principal,'Final Principal after',t,'years is:',round(newprincipal),'net gains=',round(newprincipal-principal))
