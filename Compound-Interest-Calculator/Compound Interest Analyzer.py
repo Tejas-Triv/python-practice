24389d55ad732a142809372ffac656c3ba857995
Tejas-Triv <tejastrivedi2007@gmail.com>
2026-06-30 22:09:32 +0530

    Enhance compound interest calculator with inflation adjustment
    Added inflation adjustment feature to compound interest calculation and improved output formatting.

--- Compound-Interest-Calculator/Compound Interest Analyzer.py
+++ Compound-Interest-Calculator/Compound Interest Analyzer.py
@@ -1,11 +1,21 @@
 print('Compound Interest Analyser')
 principal=float(input('enter principal amount'))
-r=float(input('enter rate of interest'))
-t=int(input('enter time in years'))
+realprincipal=principal
 newprincipal=principal
 interest=0
+r=float(input('enter rate of interest'))
+t=int(input('enter time in years'))
+ask=input('do u want to take in account for inflation?: enter y or n')#adding inflation calculation
+if ask.lower()=='y':
+    inflation=float(input('enter yearly inflation rate(in%)'))
+else:
+    inflation=0
 for i in range(0,t):
-    interest=(newprincipal*r)/100
-    newprincipal+=(newprincipal*r)/100
-    print('Year-',i+1,'New Principal=',round(newprincipal),'interest acquired this year=',round(interest),'total gains=',round(newprincipal-principal),sep='|')
-print('Initial Principal was:',principal,'Final Principal after',t,'years is:',round(newprincipal),'net gains=',round(newprincipal-principal))
+    interest=(newprincipal*r)/100 #rearranged interest and newprincipal order to display correct year interest
+    newprincipal+=interest
+    realprincipal=newprincipal*(1-(inflation/100))**(i+1) #added power^i to make code correct
+    if ask.lower()=='y':
+        print('Year-',i+1,'interest acquired=',round(interest),'New Principal(raw)=',round(newprincipal),'total gains(raw)=',round(newprincipal-principal),'inflation adjusted principal=',round(realprincipal),'net gains=',round(realprincipal-principal),sep='|')
+    else:
+        print('Year-',i+1,'interest acquired=',round(interest),'New Principal(raw)=',round(newprincipal),'total gains(raw)=',round(newprincipal-principal),sep='|')
+print('Initial Principal was:',principal,'Final Principal(raw) after',t,'years is:',round(newprincipal),'net gains(raw)=',round(newprincipal-principal),'Real Principal=',round(realprincipal),'net gains=',round(realprincipal-principal))
