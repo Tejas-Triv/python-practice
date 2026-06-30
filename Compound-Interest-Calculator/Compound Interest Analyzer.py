91974309cf49d8583093ec3ca0e27bcc9ec3fef8
Tejas-Triv <tejastrivedi2007@gmail.com>
2026-06-30 22:33:38 +0530

    Improve code readability and variable naming
    Changing Variable Names/Improved Spacing and easier terms to distinguish to improve readability

--- Compound-Interest-Calculator/Compound Interest Analyzer.py
+++ Compound-Interest-Calculator/Compound Interest Analyzer.py
@@ -1,21 +1,27 @@
-print('Compound Interest Analyser')
-principal=float(input('enter principal amount'))
-realprincipal=principal
-newprincipal=principal
-interest=0
-r=float(input('enter rate of interest'))
-t=int(input('enter time in years'))
-ask=input('do u want to take in account for inflation?: enter y or n')#adding inflation calculation
-if ask.lower()=='y':
-    inflation=float(input('enter yearly inflation rate(in%)'))
+print ('Compound Interest Analyser')
+
+principal = float (input ('enter principal amount'))
+r = float(input('enter rate of interest'))
+t = int(input('enter time in years'))
+inflation_choice = input('do u want to take in account for inflation?: enter y or n')#adding inflation calculation
+
+realprincipal = principal
+current_principal = principal
+interest = 0
+
+if inflation_choice.lower()=='y':
+    inflation = float(input('enter yearly inflation rate(in%)'))
 else:
-    inflation=0
-for i in range(0,t):
-    interest=(newprincipal*r)/100 #rearranged interest and newprincipal order to display correct year interest
-    newprincipal+=interest
-    realprincipal=newprincipal*(1-(inflation/100))**(i+1) #added power^i to make code correct
-    if ask.lower()=='y':
-        print('Year-',i+1,'interest acquired=',round(interest),'New Principal(raw)=',round(newprincipal),'total gains(raw)=',round(newprincipal-principal),'inflation adjusted principal=',round(realprincipal),'net gains=',round(realprincipal-principal),sep='|')
+    inflation = 0
+
+for year in range(1,t+1):
+    interest = current_principal * r / 100 #rearranged interest and newprincipal order to display correct year interest
+    current_principal += interest
+    realprincipal = current_principal * (1 - (inflation / 100)) ** (year) #added power^i to make code correct
+    
+    if inflation_choice.lower()=='y':
+        print ('Year-',year,'interest acquired=',round(interest),'Current Principal(raw)=',round(current_principal),'total gains(raw)=',round(current_principal-principal),'inflation adjusted principal=',round(realprincipal),'net gains=',round(realprincipal-principal),sep='|')
     else:
-        print('Year-',i+1,'interest acquired=',round(interest),'New Principal(raw)=',round(newprincipal),'total gains(raw)=',round(newprincipal-principal),sep='|')
-print('Initial Principal was:',principal,'Final Principal(raw) after',t,'years is:',round(newprincipal),'net gains(raw)=',round(newprincipal-principal),'Real Principal=',round(realprincipal),'net gains=',round(realprincipal-principal))
+        print ('Year-',year,'interest acquired=',round(interest),'Current Principal(raw)=',round(current_principal),'total gains(raw)=',round(current_principal-principal),sep='|')
+        
+print ('Initial Principal was:',principal,'Final Principal(raw) after',t,'years is:',round(current_principal),'net gains(raw)=',round(current_principal-principal),'Real Principal=',round(realprincipal),'net gains=',round(realprincipal-principal))
