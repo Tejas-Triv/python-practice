3ab4a74efbfb82c50728760b7b8eb211922204d4
Tejas-Triv <tejastrivedi2007@gmail.com>
2026-06-30 22:46:19 +0530

    Improve readability and structure of interest calculator
    Refactored inflation calculation for readability and added title line for clarity.
    reduced formulaes and converted them to variables to make it easier to read

--- Compound-Interest-Calculator/Compound Interest Analyzer.py
+++ Compound-Interest-Calculator/Compound Interest Analyzer.py
@@ -1,9 +1,14 @@
+title_line = '=' * 40 #to make code clearer
+
 print ('Compound Interest Analyser')
+print (title_line)
 
 principal = float (input ('enter principal amount'))
 r = float(input('enter rate of interest'))
 t = int(input('enter time in years'))
 inflation_choice = input('do u want to take in account for inflation?: enter y or n')#adding inflation calculation
+
+print (title_line)
 
 realprincipal = principal
 current_principal = principal
@@ -14,14 +19,19 @@ if inflation_choice.lower()=='y':
 else:
     inflation = 0
 
+inflation_factor = 1 - inflation #improve readability
+
 for year in range(1,t+1):
     interest = current_principal * r / 100 #rearranged interest and newprincipal order to display correct year interest
     current_principal += interest
-    realprincipal = current_principal * (1 - (inflation / 100)) ** (year) #added power^i to make code correct
+    realprincipal = current_principal * inflation_factor ** (year) #added power^year to make code correct
     
     if inflation_choice.lower()=='y':
         print ('Year-',year,'interest acquired=',round(interest),'Current Principal(raw)=',round(current_principal),'total gains(raw)=',round(current_principal-principal),'inflation adjusted principal=',round(realprincipal),'net gains=',round(realprincipal-principal),sep='|')
     else:
         print ('Year-',year,'interest acquired=',round(interest),'Current Principal(raw)=',round(current_principal),'total gains(raw)=',round(current_principal-principal),sep='|')
-        
+
+    print (title_line)
+
 print ('Initial Principal was:',principal,'Final Principal(raw) after',t,'years is:',round(current_principal),'net gains(raw)=',round(current_principal-principal),'Real Principal=',round(realprincipal),'net gains=',round(realprincipal-principal))
+print (title_line)
